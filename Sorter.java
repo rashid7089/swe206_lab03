e538ad4529ef2514f2ddd34f30cb1fb897fbfec1
Marwanshtine <eng.mrwanm@gmail.com>
2023-09-19 16:47:18 +0300

    Added the insertionSort method

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
 public class Sorter {
     public static void main(String[] args) {
-        
+
+    }
+    public static void insertionSort(ArrayList<Integer> arr) {
+        for(int i = 1; i < arr.size(); i++) {
+            int value = arr.get(i);
+            int j;
+            for(j = i; j > 0 && value < arr.get(j - 1); j--)
+                arr.set(j, arr.get(j - 1));
+            arr.set(j, value);
+        }
     }
 }
